c32c227a7603818300e842fb81c37b29c1a65d36
Nishant saini <ns0109375@gmail.com>
2024-10-29 15:02:33 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,11 @@ const app = express();
 const cors = require("cors");
 const port = PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/v1", userRoute);
 app.use("/api/v1", eventRoute);
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
